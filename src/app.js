97afb3f8a5b77312391accfbbe9d32b59cb75412
EuLeEr <eermolov@mail.ru>
2019-08-24 15:49:03 +0300

    lesson 55

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
-/* import './utils.js';
-
-console.log("This is App.js from Webpack "); */
-
-/*import isSenior, {isAdult, canDrink} from "./person.js";
-
-console.log(isAdult(57) , canDrink(18), isSenior(65) );*/
-
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndesicionApp';
 
-const Nax = <p> Eshki-Moshki </p>;
-ReactDom.render(Nax, document.getElementById('app'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- src/person.js
+++
@@ -1,3 +0,0 @@
-export const isAdult = ( years ) =>  (years <= 18 ) ?  true  : false;
-export const canDrink = ( years) =>  (years <= 18 ) ?  true  : false;
-export default  ( years) =>  (years > 64 ) ;
