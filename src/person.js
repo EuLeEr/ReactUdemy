eeee1d9223293597a5912aa52ee60d2b0095516d
EuLeEr <eermolov@mail.ru>
2019-08-24 12:19:41 +0300

    Now garmonized webpack.config.js , .babel.rc and package.json

---
+++ src/person.js
@@ -0,0 +1,3 @@
+export const isAdult = ( years ) =>  (years <= 18 ) ?  true  : false;
+export const canDrink = ( years) =>  (years <= 18 ) ?  true  : false;
+export default  ( years) =>  (years > 64 ) ;
---
+++ src/utils.js
@@ -0,0 +1,1 @@
+console.log("Ulils.js running");
